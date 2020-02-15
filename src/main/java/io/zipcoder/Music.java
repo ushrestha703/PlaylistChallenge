1a1432ba594de7dee4a2a8878d16de4f8de1c3e0
Ujjwal Shrestha <ushrestha@zipcodes-MBP.fios-router.home>
2020-02-15 15:27:04 -0500

    Code compiles. test pending

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -1,14 +1,54 @@
 package io.zipcoder;
 
-public class Music {
+import com.sun.deploy.security.SelectableSecurityManager;
+
+public class Music
+{
 
     private String[] playList;
 
     public Music(String[] playList){
+
         this.playList = playList;
     }
 
-    public Integer selection(Integer startIndex, String selection){
-        return null;
+    public Integer selection(Integer startIndex, String selection)
+    {
+
+        int nextSong = 0;
+        int previousSong = 0;
+        int Index = 0;
+
+        for (int i = 0; i < playList.length; i++)
+        {
+            if ( selection == playList[i])
+            {
+                Index = i;
+
+                if (Index > startIndex)
+                {
+                    nextSong = Index - startIndex;
+                    if (Index < playList.length - 1)
+                    {
+                    previousSong = playList.length - 1 - Index;
+                    }
+                }
+             }
+          }
+                if (nextSong > previousSong && previousSong != 0)
+                 {
+                        return previousSong;
+                 }
+                else
+                 {
+                        return nextSong;
+
+                 }
+
+
     }
+
+
 }
+
+
